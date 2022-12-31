f393f5ec3b59281a4aa61d5c780ac3d514ab8388
GeonNamGung <rndrjs123@naver.com>
2023-01-01 00:17:40 +0900

    feat: add effect when router.query is updated
    feat: add popstate listener

--- components/AlertWhenRouterIsUpdated.jsx
+++ components/AlertWhenRouterIsUpdated.jsx
@@ -3,11 +3,19 @@ import React, { useEffect } from "react";
 
 export const AlertWhenRouterIsUpdated = () => {
   const router = useRouter();
-  !globalThis.window && console.log("router?", router);
+
   useEffect(() => {
-    alert("router is updated(or init)");
     console.log("router is updated(or init)", router);
   }, [router]);
+  useEffect(() => {
+    console.log("router.query is updated(or init)", router.query);
+  }, [router.query]);
+
+  useEffect(() => {
+    window.addEventListener("popstate", (e) => {
+      console.log("popstate!", e);
+    });
+  }, []);
 
   return <h1>Currennt path: {router.asPath}</h1>;
 };
